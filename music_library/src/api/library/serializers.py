d1b933e181e8c38bcf75b41debb96cc47f1db673
ivansizov <sizovivan.05@gmail.com>
2024-03-16 03:35:15 +0700

    Code optimization p2 and Artist api view

--- music_library/src/api/library/serializers.py
+++ music_library/src/api/library/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 
 from core.services import filter_objects
-from src.music_app.models import Album, Track
+from src.music_app.models import Album, Track, Artist, Genre
 
 
-# TODO Think about optimized serializer
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('id', 'name')
+
+
+class ArtistSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer()
+
+    class Meta:
+        model = Artist
+        fields = ['id', 'name', 'country', 'avatar', 'genre']
+
+
+
 class AlbumSerializer(serializers.ModelSerializer):
     artist = serializers.StringRelatedField()
     tracks = serializers.SerializerMethodField()
@@ -14,7 +28,9 @@ class AlbumSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'release_year', 'artist', 'biography', 'cover_image', 'tracks']
 
     def get_tracks(self, instance):
-        tracks = filter_objects(obj=Track.objects.filter(album=instance), album=instance, prefetch_related=('artist',))
+        tracks = filter_objects(obj=Track.objects.filter(album=instance),
+                                album=instance,
+                                prefetch_related=('artist',))
         return TrackSerializer(tracks, many=True).data
 
 
