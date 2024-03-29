632b043c1de1724971dc0018120d3082fac42c12
Ivan <sizovivan.05@gmail.com>
2024-03-29 12:13:01 +0700

    Redis installation

--- music_library/music_library/settings.py
+++ music_library/music_library/settings.py
@@ -93,6 +93,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/0"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 
 REST_FRAMEWORK = {
